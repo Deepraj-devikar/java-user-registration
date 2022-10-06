ccb94caa5c0d7697a91ba2a613cdfa1a73ae4ee4
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-10-06 17:42:53 +0530

    ADDED: added pattern for email validation and validate email

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -34,6 +34,39 @@ public class UserRegistration {
 		} else {
 			System.out.println("FAIL");
 		}
+		
+		// Email should compulsory contains three parts that are username, domain, top level domain
+		// email address will like this username@domain.top level domain
+		// -----------------------------------------------------------------------------
+		// Formation of username :- ^(?!.*\\.\\.)[^.][a-zA-Z0-9+_.-]+[^.]
+		// username dose not contains double dots it is solved with (?!.*\\.\\.)
+		// username dose not start with dot it is solved with [^.] as first character
+		// username should contain small letters, capital letters, digits, underscore, dot, dash only is done with [a-zA-Z0-9+_.-]
+		// + is use to reffer occurs once or more times
+		// username dose not end with dot it is solved with [^.] as last character
+		// after username email will contain @ and after @ sign domain will start
+		// -------------------------------------------------------------------------------
+		// Formation of domain :- [a-zA-Z0-9&&[^.]][a-zA-Z0-9.]*
+		// domain should contains small letters, capital letters, digits and domains first character can not be dot
+		// we use [a-zA-Z0-9&&[^.]] in domain as first character which means first character can be
+		// small letter, capital letter, digit and not be dot
+		// [a-zA-Z0-9.]* it means small letter, capital letter, digit, dot any character in between these with occurrence of zero or more times
+		// after domain email will contain dot sign and after dot sign top level domain will start
+		// dot is mentioned in pattern as [\\.]{1} (i.e. one occurrence of dot)
+		// ----------------------------------------------------------------------------------
+		// Formation of top level domain :- [a-zA-z]{2,}
+		// top level domain will contain small letters, capital letters with length 2 or more than 2
+		Pattern emailPattern = Pattern.compile("^(?!.*\\.\\.)[^.][a-zA-Z0-9+_.-]+[^.]@[a-zA-Z0-9&&[^.]][a-zA-Z0-9.]*[\\.]{1}[a-zA-z]{2,}$");
+
+		// Email validation according to email pattern
+		System.out.print("Enter email : ");
+		String email = scanner.nextLine();
+		System.out.print("Validation for email '"+email+"' is : ");
+		if(emailPattern.matcher(email).matches()) {
+			System.out.println("PASS");
+		} else {
+			System.out.println("FAIL");
+		}
 	}
 
 }
