7d41ce71ee01148f07ad790082d9d1713464d0e1
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-10-06 16:11:39 +0530

    ADDED: start project user registration with welcome message

---
+++ src/com/userregistration/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.userregistration;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to user registration program");
+
+	}
+
+}
